007385255b562073ddad29a4cbee6e9e9ac8ed04
vivganes <caliberoviv@gmail.com>
2025-08-13 04:47:33 +0530

    fix #222

--- internal/engine/stubs_test.go
+++ internal/engine/stubs_test.go
@@ -73,6 +73,8 @@ func loadFixture(fixture, fromPackage string) (fstest.MapFS, gomodule.GoModule, 
 }
 
 func filenameFromFixture(fix string) string {
+	// Replace the path separator with the OS specific one.
+	fix = strings.ReplaceAll(fix, "/", string(os.PathSeparator))
 	return strings.ReplaceAll(fix, "_go", ".go")
 }
 
